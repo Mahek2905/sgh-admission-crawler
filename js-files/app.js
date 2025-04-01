4fb43df548652277d49521a8f2f7ed7f6ae30d02
Mahek <152728593+Mahek2905@users.noreply.github.com>
2025-04-01 16:53:56 +0530

    levels and locations

--- js-files/app.js
+++ js-files/app.js
@@ -1,6 +1,41 @@
-let DATA = "http://json-schema.org/draft-04/schema#";
+const levelSelect = document.querySelector(".level-selection select");
+const stateSelect = document.querySelector(".location-selection select");
+const clgBoxes = document.querySelectorAll(".clg-box");
+const clgBody = document.querySelector(".clg-list");
+const stateNames = document.querySelectorAll(".state-name");
+const clgLevels = document.querySelectorAll(".clg-level");
+const searchInput = document.querySelector("#input-box");
 
-const myFun = async () => {
-    let var1 = await fetch(DATA);
-    console.log(var1.properties[4]);
+const filterLevels = (selectedLevel) => {
+    clgLevels.forEach((clgLevel) => {
+        let fullDiv = clgLevel.parentElement.parentElement.parentElement;
+        fullDiv.style.display = "block";
+
+        if(selectedLevel !== clgLevel.innerText.toLowerCase()) {
+            fullDiv.style.display = "none";
+        }
+    });
 }
+
+const filterStates = (selectedState) => {
+    stateNames.forEach((stateName) => {
+        let fullDiv = stateName.parentElement.parentElement.parentElement;
+        fullDiv.style.display = "block";
+
+        if(selectedState !== stateName.innerText.toLowerCase()) {
+            fullDiv.style.display = "none";
+        }
+    })
+}
+
+levelSelect.addEventListener("change", (evt) => {
+    let selectedLevel = evt.target.value.toLowerCase();
+    filterLevels(selectedLevel);
+});
+
+stateSelect.addEventListener("change", (evt) => {
+    let selectedState = evt.target.value.toLowerCase();
+    filterStates(selectedState);
+});
+
+// searchInput.addEventListener("input", (value) => console.log(value));
